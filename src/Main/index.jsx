51da285f6f21b164c63ce6cc2025289427ff870d
Vanderson <vandersontavares32@gmail.com>
2023-02-07 19:39:00 -0300

    sidebar, explorer done

--- src/Main/index.jsx
+++ src/Main/index.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { useState } from "react";
+
+import { ReactComponent as ReactIcon } from "../util/img/react_icon.svg";
+import { ReactComponent as HtmlIcon } from "../util/img/html_icon.svg";
+import { ReactComponent as CssIcon } from "../util/img/css_icon.svg";
+import { ReactComponent as JsIcon } from "../util/img/js_icon.svg";
+import { ReactComponent as JsonIcon } from "../util/img/json_icon.svg";
 
 import {
   VscFiles,
+  VscChevronRight,
+  VscChevronDown,
+  VscEllipsis,
   VscSearch,
   VscSourceControl,
   VscDebugAlt,
@@ -9,17 +19,18 @@ import {
   VscAccount,
   VscGear,
 } from "react-icons/vsc";
-import { AiFillHtml5 } from "react-icons/ai";
 
 import "./styles.css";
 
 const Main = () => {
+  const [open, setOpen] = useState(true);
+
   return (
     <div className="grid-container">
       <div className="sidebar left">
         <div className="top-buttons">
           <div className="icon-config">
-            <VscFiles />
+            <VscFiles className="active"/>
           </div>
           <div className="icon-config">
             <VscSearch />
@@ -44,16 +55,43 @@ const Main = () => {
         </div>
       </div>
       <div className="explorer right">
-        <div class="dropdown">
-          <button onclick="myFunction()" class="dropbtn">
-            Dropdown
-          </button>
-          <div id="myDropdown" class="dropdown-content">
-            <a href="#">Link 1</a>
-            <a href="#">Link 2</a>
-            <a href="#">Link 3</a>
-          </div>
+        <div className="explorer-div">
+          <h4>EXPLORER</h4>
+            <VscEllipsis className="explorer-icon"/>
         </div>
+        <button className="explorer-btn" onClick={()=> setOpen(!open)}> {open ? <VscChevronDown className="arrow-down"/> : <VscChevronRight/>} PORTFOLIO</button>
+        {open && <div className="explorer-content">
+          <div className="item">
+            <a href="#">
+              <ReactIcon className="explorer-icon" />
+              <p>home.jsx</p>
+            </a>
+          </div>
+          <div className="item">
+            <a href="#">
+              <HtmlIcon className="explorer-icon" />
+              <p>about.html</p>
+            </a>
+          </div>
+          <div className="item">
+            <a href="#">
+              <CssIcon className="explorer-icon" />
+              <p>contact.css</p>
+            </a>
+          </div>
+          <div className="item">
+            <a href="#">
+              <JsIcon className="explorer-icon" />
+              <p>projects.js</p>
+            </a>
+          </div>
+          <div className="item">
+            <a href="#">
+              <JsonIcon className="explorer-icon" />
+              <p>github.md</p>
+            </a>
+          </div>
+        </div>}
       </div>
       <div className="content r-right">
         <p>content</p>
