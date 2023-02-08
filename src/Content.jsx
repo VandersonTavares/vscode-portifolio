ca653fd328bf79b967420c777adb2df099f65d04
Vanderson <vandersontavares32@gmail.com>
2023-02-08 02:25:27 -0300

    error solved

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Header from "./Header";
 import Main from "./Main";
