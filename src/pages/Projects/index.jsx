c4bb96eff6c8f93563b5fda79f4acde093fae562
Vanderson <vandersontavares32@gmail.com>
2023-02-12 13:46:26 -0300

    project-pages updated

--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -10,7 +10,7 @@ const Projects = () => {
     <div className='layout-content'>
       <h3>Projetinho Fellas</h3>  
       <div className="project-content">
-          <ProjectCard name={"Cellshop"} description={"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid saepe corrupti"} demoLink={"https://stalwart-lily-2c6a43.netlify.app/"} gitLink={"https://github.com/VandersonTavares/cellshop"}/>
+          <ProjectCard name={"Cellshop"} description={"Projeto criado utilizando a API do Mercado Livre, Axios, Local Storage e Styled Components."} demoLink={"https://stalwart-lily-2c6a43.netlify.app/"} gitLink={"https://github.com/VandersonTavares/cellshop"}/>
           <ProjectCard name={"Vs-Code Protfolio"} description={"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid saepe corrupti"} demoLink={"https://tangerine-paprenjak-2b39d8.netlify.app/"} gitLink={"https://github.com/VandersonTavares/vscode-portifolio"}/>
           <ProjectCard name={"Cellshop"} description={"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid saepe corrupti"} demoLink={"https://stalwart-lily-2c6a43.netlify.app/"} gitLink={"https://github.com/VandersonTavares/cellshop"}/>
       </div>
