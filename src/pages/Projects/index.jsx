c598690fbe501138145c7a6687544c200944eb30
Vanderson <vandersontavares32@gmail.com>
2023-02-17 20:33:45 -0300

    new project added

--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -13,6 +13,7 @@ const Projects = () => {
           <ProjectCard name={"Cellshop"} description={"Projeto criado utilizando a API do Mercado Livre, Axios, Local Storage e Styled Components."} demoLink={"https://cellshop-demo.netlify.app/"} gitLink={"https://github.com/VandersonTavares/cellshop"}/>
           <ProjectCard name={"Vs-Code Protfolio"} description={"Portfolio inspirado na IDE Visual Code Studio, projeto de portfolio pessoal."} demoLink={"https://vanderson-tavares-portfolio.netlify.app/"} gitLink={"https://github.com/VandersonTavares/vscode-portifolio"}/>
           <ProjectCard name={"Buscador de Usuário Github"} description={"Pequeno desafio feito no bootcamp da DevSuperior, ensinando como consumir API's."} demoLink={"https://buscador-github-api.netlify.app/"} gitLink={"https://github.com/VandersonTavares/desafio-github"}/>
+          <ProjectCard name={"Twitter Clone"} description={"Projeto criado utlizando typescript no react, react-icons"} demoLink={"#/"} gitLink={"https://github.com/VandersonTavares/twitter-clone"}/>
       </div>
     </div>
   )
