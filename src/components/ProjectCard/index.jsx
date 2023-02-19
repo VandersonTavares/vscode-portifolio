4eb570f79796c0199518740164832d511d5392fa
Vanderson <vandersontavares32@gmail.com>
2023-02-18 21:30:01 -0300

    projects page updated

--- src/components/ProjectCard/index.jsx
+++ src/components/ProjectCard/index.jsx
@@ -3,20 +3,22 @@ import { NavLink } from 'react-router-dom';
 
 import "./styles.css";
 
-const ProjectCard = ({name, description, gitLink, demoLink}) => {
+import Techlogo from "../Techlogo";
+
+
+const ProjectCard = ({ name, description, gitLink, demoLink, image }) => {
   return (
     <div className="card">
         <div className="img">
+          <img src={image} alt="project-img" />
         </div>
         <div className="project-infos">
             <h3><NavLink to={demoLink} target={"_blank"}>{name}</NavLink></h3>
             <p>{description}</p>
         </div>
-        <div className="project-tags">
-            <span>ReactJS</span>
-            <span>HTML5</span>
-            <span>CSS3</span>
-        </div>
+
+        <Techlogo />
+        
         <div className="project-urls">
             <NavLink to={gitLink} target={"_blank"}>Código Fonte</NavLink>
         </div>
--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -4,16 +4,24 @@ import "./styles.css";
 
 import ProjectCard from '../../components/ProjectCard';
 
+import TwitterImg from "../../util/img/twitter.png";
+import VsCodeImg from "../../util/img/vscode-img.png";
+import GithubImg from "../../util/img/github-cover.jpg";
+import CellphoneImg from "../../util/img/cellshopbanner.png";
+import FoodCommerce from "../../util/img/food-commerce.png";
+
+
 const Projects = () => {
   return (
 
     <div className='layout-content'>
       <h3>Meus Projetos</h3>  
       <div className="project-content">
-          <ProjectCard name={"Cellshop"} description={"Projeto criado utilizando a API do Mercado Livre, Axios, Local Storage e Styled Components."} demoLink={"https://cellshop-demo.netlify.app/"} gitLink={"https://github.com/VandersonTavares/cellshop"}/>
-          <ProjectCard name={"Vs-Code Protfolio"} description={"Portfolio inspirado na IDE Visual Code Studio, projeto de portfolio pessoal."} demoLink={"https://vanderson-tavares-portfolio.netlify.app/"} gitLink={"https://github.com/VandersonTavares/vscode-portifolio"}/>
-          <ProjectCard name={"Buscador de Usuário Github"} description={"Pequeno desafio feito no bootcamp da DevSuperior, ensinando como consumir API's."} demoLink={"https://buscador-github-api.netlify.app/"} gitLink={"https://github.com/VandersonTavares/desafio-github"}/>
-          <ProjectCard name={"Twitter Clone"} description={"Projeto criado utlizando typescript no react, react-icons"} demoLink={"#/"} gitLink={"https://github.com/VandersonTavares/twitter-clone"}/>
+          <ProjectCard name={"Vs-Code Protfolio"} image={VsCodeImg} description={"Portfolio inspirado na IDE Visual Code Studio, projeto de portfolio pessoal."} demoLink={"https://vanderson-tavares-portfolio.netlify.app/"} gitLink={"https://github.com/VandersonTavares/vscode-portifolio"}/>
+          <ProjectCard name={"Food Commerce"} image={FoodCommerce} description={"NÃO FINALIZADO"} demoLink={"#"} gitLink={"https://github.com/VandersonTavares/Pedido-Online"}/>
+          <ProjectCard name={"Buscador de Usuário Github"}image={GithubImg} description={"Pequeno desafio feito no bootcamp da DevSuperior, ensinando como consumir API's."} demoLink={"https://buscador-github-api.netlify.app/"} gitLink={"https://github.com/VandersonTavares/desafio-github"}/>
+          <ProjectCard name={"Twitter Clone"} image={TwitterImg} description={"Projeto criado utlizando typescript, react-icons"} demoLink={"https://uitwitter-clone.netlify.app"} gitLink={"https://github.com/VandersonTavares/twitter-clone"}/>
+          <ProjectCard name={"Cellshop"} image={CellphoneImg} description={"Projeto criado utilizando a API do Mercado Livre, Axios, Local Storage e Styled Components."} demoLink={"https://cellshop-demo.netlify.app/"} gitLink={"https://github.com/VandersonTavares/cellshop"}/>
       </div>
     </div>
   )
