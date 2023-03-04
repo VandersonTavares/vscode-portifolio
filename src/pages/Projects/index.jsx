72c829fdacd4c32a0a47d4de1589f705db9edb73
Vanderson <vandersontavares32@gmail.com>
2023-03-04 18:50:49 -0300

    project updated

--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -10,62 +10,71 @@ import GithubImg from "../../util/img/github-cover.jpg";
 import CellphoneImg from "../../util/img/cellshopbanner.png";
 import FoodCommerce from "../../util/img/food-commerce.png";
 import MusicApp from "../../util/img/musicapp.png";
-import HydraPage from "../../util/img/hydra-page.jpg"
-
+import HydraPage from "../../util/img/hydra-page.jpg";
+import Pokedex from "../..//util/img/pokedex.jpg";
 
 const Projects = () => {
   return (
     <div className="layout-content">
       <h3>Meus Projetos</h3>
       <div className="project-content">
-      <ProjectCard
+        <ProjectCard
           name={"Hydra Landing Page"}
           image={HydraPage}
-          technologies = {['react', 'html', 'css', 'tailwind']}
-          description={"NÃO FINALIZADO - Hydra Landing Page, projeto free da comunidade do Figma, link no repositorio."}
+          technologies={["react", "html", "css", "tailwind"]}
+          description={
+            "NÃO FINALIZADO - Hydra Landing Page, projeto free da comunidade do Figma, link no repositorio."
+          }
           demoLink={"https://hydra-landingpage.netlify.app"}
           gitLink={"https://github.com/VandersonTavares/hydra-page"}
         />
         <ProjectCard
           name={"Music App"}
           image={MusicApp}
-          technologies={["react", "html", "css", "tailwind" ]}
-          description={
-            "NÃO FINALIZADO"
-          }
+          technologies={["react", "html", "css", "tailwind"]}
+          description={"NÃO FINALIZADO"}
           demoLink={"#"}
           gitLink={"https://github.com/VandersonTavares/musicapp"}
         />
-          <ProjectCard
-            name={"Vs-Code Protfolio"}
-            image={VsCodeImg}
-            technologies = {['react', 'html', 'css']}
-            description={
-              "NÃO FINALIZADO - Portfolio inspirado na IDE Visual Code Studio, projeto de portfolio pessoal."
-            }
-            demoLink={"https://vanderson-tavares-portfolio.netlify.app/"}
-            gitLink={"https://github.com/VandersonTavares/vscode-portifolio"}
-          />
+
+        <ProjectCard
+          name={"Vs-Code Protfolio"}
+          image={VsCodeImg}
+          technologies={["react", "html", "css"]}
+          description={
+            "NÃO FINALIZADO - Portfolio inspirado na IDE Visual Code Studio, projeto de portfolio pessoal."
+          }
+          demoLink={"https://vanderson-tavares-portfolio.netlify.app/"}
+          gitLink={"https://github.com/VandersonTavares/vscode-portifolio"}
+        />
         <ProjectCard
           name={"Food Commerce"}
           image={FoodCommerce}
-          technologies = {['react', 'html', 'css', 'typescript']}
+          technologies={["react", "html", "css", "typescript"]}
           description={"NÃO FINALIZADO"}
           demoLink={"#"}
           gitLink={"https://github.com/VandersonTavares/Pedido-Online"}
         />
-          <ProjectCard
-            name={"Twitter Clone"}
-            image={TwitterImg}
-            technologies={["react", "html", "css", "typescript" ]}
-            description={"Projeto criado utlizando typescript, react-icons"}
-            demoLink={"https://hydra-landingpage.netlify.app/"}
-            gitLink={"https://github.com/VandersonTavares/twitter-clone"}
-          />
+        <ProjectCard
+          name={"Twitter Clone"}
+          image={TwitterImg}
+          technologies={["react", "html", "css", "typescript"]}
+          description={"Projeto criado utlizando typescript, react-icons"}
+          demoLink={"https://hydra-landingpage.netlify.app/"}
+          gitLink={"https://github.com/VandersonTavares/twitter-clone"}
+        />
+        <ProjectCard
+          name={"Pokedéx"}
+          image={Pokedex}
+          technologies={["react", "html", "css", "tailwind"]}
+          description={"NÃO FINALIZADO - Utilizando a API PokeApi"}
+          demoLink={""}
+          gitLink={"https://github.com/VandersonTavares/pokedex"}
+        />
         <ProjectCard
           name={"Buscador de Usuário Github"}
           image={GithubImg}
-          technologies={[ "react", "html", "css"]}
+          technologies={["react", "html", "css"]}
           description={
             "Pequeno desafio feito no bootcamp da DevSuperior, ensinando como consumir API's."
           }
@@ -75,7 +84,7 @@ const Projects = () => {
         <ProjectCard
           name={"Cellshop"}
           image={CellphoneImg}
-          technologies={["react", "html", "css" ]}
+          technologies={["react", "html", "css"]}
           description={
             "Projeto criado utilizando a API do Mercado Livre, Axios, Local Storage e Styled Components."
           }
